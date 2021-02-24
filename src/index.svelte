ec78282fd8390f670e9f3ab5c69a51e020494fb2
ZAUB1 <aub1@protonmail.ch>
2021-02-24 14:54:29 +0100

    init

---
+++ src/index.svelte
@@ -0,0 +1,28 @@
+<div id="main">
+    <div id="gjs">
+        <h1>Hello World Component!</h1>
+    </div>
+</div>
+
+<script>
+import { onMount } from 'svelte';
+import 'grapesjs/dist/css/grapes.min.css';
+import grapesjs from "grapesjs";
+
+onMount(() => {
+    const editor = grapesjs.init({
+        container: '#gjs',
+        fromElement: true,
+        storageManager: false
+    });
+});
+</script>
+
+<style>
+    #main {
+        position: absolute;
+        display: inline;
+        min-width: 100%;
+        min-height: 100%;
+    }
+</style>
