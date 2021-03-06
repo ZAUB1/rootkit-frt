23881e7efe3b1ff4187276f96f42888e1234a79c
ZAUB1 <aub1@protonmail.ch>
2021-03-06 14:10:54 +0100

    feature: working navbar

--- src/components/Navbar/index.ts
+++ src/components/Navbar/index.ts
@@ -6,6 +6,6 @@ import { Component } from "../../core/controllers/component";
 export default new Component(
     "Navbar", // Name
     content, // Contenu html
-    {  }, // Attributs grapesjs (icones drawer)
-    { style } // Attributs de notre framwork (style, traits, ...)
+    { class: "gjs-fonts", content: "A" }, // Attributs grapesjs (icones drawer)
+    { style, category: "Requirements" } // Attributs de notre framwork (style, traits, ...)
 );
