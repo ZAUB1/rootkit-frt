4036a2b69860a9798dc7c17a94657935a9afccbe
ZAUB1 <aub1@protonmail.ch>
2021-03-06 16:33:29 +0100

    feature: working vars on comp

--- src/components/Text/index.ts
+++ src/components/Text/index.ts
@@ -1,4 +1,25 @@
 import body from "./body.html";
 import { Component } from "../../core/controllers/component";
 
-export default new Component("Text", body, { class: 'gjs-fonts gjs-f-text' }, { traits: ["name"] });
+export default new Component(
+    "Text",
+    body,
+    { class: 'gjs-fonts gjs-f-text' },
+    {
+        traits: [{
+            type: "select",
+            label: "Weight",
+            name: "weight",
+            options: [
+                { id: "normal", name: "Normal" },
+                { id: "bold", name: "Bold" }
+            ],
+            cb: (_this: any, el: any) => {
+                _this.rebuildContent(el);
+            }
+        }],
+        vars: {
+            weight: "bold"
+        }
+    }
+);
--- src/core/controllers/component.ts
+++ src/core/controllers/component.ts
@@ -1,24 +1,68 @@
+import { EventEmitter } from "events";
+
 import { editor } from "../editor";
 import { parseStyle } from "../style";
 
-export class Component {
+export class Component extends EventEmitter {
     public id: string;
     public label: string;
     public category: string;
     public content: string | object;
     public attributes: any;
+    private vars: any = {};
+    private style: any = {};
+    private traits: any = {};
+    private baseContent: string;
 
-    public constructor(label: string, content: string | object, attributes: object = {}, { style, traits = [], category = "Default" }: any = {}) {
+    public rebuildContent(el: any) {
+        let content = this.baseContent.replace(/\{ (.*?) \}|\{(.*?)\}/, (sub: string, ...args: any[]): any => {
+            sub = sub.split("{")[1].split("}")[0].replace(/\s/g, "");
+            console.log(sub);
+            if (this.vars[sub])
+                return this.vars[sub];
+        });
+
+        this.content = (this.style) ? `<style>${parseStyle(this.style)}</style>${content}` : content;
+        this.content = `<${this.label.toLowerCase()}>${this.content}</${this.label.toLowerCase()}>`
+        el.innerHTML = this.content;
+    };
+
+    private updateAttributesHandler(_: any) {
+        const attributes = _.changed.attributes;
+        const el = _.view.el;
+        const foundTrait = this.traits.find((trait: any) => trait.name == Object.keys(attributes)[0]);
+        if (!foundTrait)
+            return;
+        this.vars[foundTrait.name] = attributes[foundTrait.name];
+        foundTrait.cb(this, el);
+    };
+
+    public constructor(label: string, content: string | object, attributes: object = {}, { style, traits = [], category = "Default", vars = {} }: any = {}) {
+        super();
+        const _this = this;
         this.id = label;
         this.label = label;
         this.category = category;
+        this.vars = vars;
+        this.style = style;
+        this.traits = traits;
+        this.baseContent = content as string;
+        this.attributes = attributes;
+
         if (typeof content == "string") {
+            content = content.replace(/\{ (.*?) \}|\{(.*?)\}/, (sub: string, ...args: any[]): any => {
+                sub = sub.split("{")[1].split("}")[0].replace(/\s/g, "");
+                if (this.vars[sub])
+                    return this.vars[sub];
+                return sub;
+            });
+
             this.content = (style) ? `<style>${parseStyle(style)}</style>${content}` : content;
             this.content = `<${label.toLowerCase()}>${this.content}</${label.toLowerCase()}>`
         } else {
             this.content = content;
         }
-        this.attributes = attributes;
+
         const baseModel = editor.DomComponents.getType("default");
         if (typeof content == "string") {
             editor.DomComponents.addType(label.toLowerCase(), {
@@ -28,9 +72,7 @@ export class Component {
                         traits
                     },
                     init() {
-                        this.on("change:attributes", (_: any) => {
-                            const attributes = _.attributes.attributes;
-                        });
+                        this.on("change:attributes", (_: any) => _this.updateAttributesHandler(_));
                     }
                 },
                 isComponent: (el: any) => {
@@ -39,6 +81,12 @@ export class Component {
                 }
             });
         }
-        editor.BlockManager.add(label, this);
+
+        editor.BlockManager.add(label, {
+            id: this.id,
+            label: this.label,
+            content: this.content,
+            attributes: attributes
+        });
     };
 };
