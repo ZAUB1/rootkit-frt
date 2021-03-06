3cb9cf085936fd45090de164382a1d26fad8bbaf
MarcRichet <mrichet.cb@gmail.com>
2021-03-06 14:12:01 +0100

    feature: Create and config Dropdown component

---
+++ src/components/Dropdown/style.ts
@@ -0,0 +1,18 @@
+export default {
+//     dropdownContainer: {
+//         display: "inline-flex",
+//         height: "auto",
+//         width: "20px",
+//         padding: "0 16px",
+//         backgroundColor: "#d7d7d7",
+//         borderRadius: "4px",
+//         color: "rgba(0,0,0,.87)",
+//         cursor: "pointer",
+//         userSelect: "none",
+//         textDecoration: "none",
+//         letterSpacing: ".0892857143em",
+//         textIndent: ".0892857143em",
+//         textTransform: "uppercase",
+//         fontWeight: "500",
+//     },
+}
