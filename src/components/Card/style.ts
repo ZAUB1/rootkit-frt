4481e49d70ebdb0c1f86a59d862e94e192dccf40
MarcRichet <mrichet.cb@gmail.com>
2021-03-08 09:25:54 +0100

    chore: little css stuff

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -2,16 +2,16 @@ export default {
     cardContainer: {
         display: "inline-flex",
         flexDirection: "column",
-        height: "200px",
+        height: "300px",
         width: "300px",
         padding: "0 auto",
         backgroundColor: "#d7d7d7",
         borderRadius: "4px",
         color: "rgba(0, 0, 0, .87)",
     },
-    cardCustom: {
+    cardSettings: {
         display: "flex",
         flexDirection: "column",
-        alignItems: "flex-start"
+        justifyContent: "center"
     }
 }
