c510994bf80beefde36ad736b035bdf5f2e36ab6
ZAUB1 <aub1@protonmail.ch>
2021-03-13 11:58:21 +0100

    fix: final viewer

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,11 @@
 import './index.scss';
+import "./components";
 
-// import "./components/Map";
 import Text from "./components/Text";
-import Simple from "./components/SimpleColumn";
-import "./components/DoubleColumn";
-import "./components/TripleColumn";
-import Button from "./components/Button";
-// import "./components/Image";
-import "./components/Navbar";
-import "./components/Dropdown";
-import "./components/Card";
 
 import "./core/router";
 import Viewer from './core/viewer';
 
 Viewer.onLoad((viewer: Viewer) => {
-    viewer.createComp(Button);
+    viewer.createComp(Text);
 });
