83626fb6dacb27356472b24e92c88f838ab16c64
ZAUB1 <aub1@protonmail.ch>
2021-03-20 12:40:37 +0100

    feature: CardProduct vars

--- src/components/Interacts/CardProduct/index.ts
+++ src/components/Interacts/CardProduct/index.ts
@@ -1,8 +1,26 @@
 import body from "./body.html";
 
 import { Component } from "../../../core/controllers/component";
-import { Category, Icon, Vars } from "../../../core/controllers";
+import { Category, Icon, Traits, Vars } from "../../../core/controllers";
 
+@Vars({
+    title: "Card title",
+    price: 20,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis elit nunc, vitae vehicula est sodales eu."
+})
+@Traits([{
+    type: "text",
+    label: "Titre",
+    name: "title",
+}, {
+    type: "number",
+    label: "Prix",
+    name: "price"
+}, {
+    type: "text",
+    label: "Description",
+    name: "desc"
+}])
 @Category("Interacts")
 @Icon("fas fa-square")
 class CardProduct extends Component {
