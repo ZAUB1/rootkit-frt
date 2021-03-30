be2e0f74dcf75a203ce80f9f70b820a03e8eacbb
zicokiller <mrichet.cb@gmail.com>
2021-03-30 14:45:40 +0200

    feature: Set components Traits

--- src/components/Interacts/Tooltip/index.ts
+++ src/components/Interacts/Tooltip/index.ts
@@ -1,10 +1,47 @@
 import body from "./body.html";
 
 import { Component } from "../../../core/controllers/component";
-import { Category, Icon } from "../../../core/controllers/decorators/editor";
+import { Category, Icon, Traits } from "../../../core/controllers/decorators/editor";
+import { Vars } from "../../../core/controllers/decorators/component";
 
-@Category("Interacts")
+@Vars({
+    body: "Text",
+    backgroundColor: "#d7d7d7",
+    borderRadius: "0px",
+    tooltip1: "tooltip",
+    tooltip2: "tooltip",
+    tooltip3: "tooltip",
+    tooltip4: "tooltip",
+})
 @Icon("fas fa-exclamation", "#e5383b")
+@Category("Interacts")
+@Traits([{
+    type: "text",
+    name: "logo",
+    label: "Logo",
+},{
+    type: "text",
+    name: "link",
+    label: "Liens",
+},{
+    type: "text",
+    name: "link",
+    label: "Liens",
+},{
+    type: "text",
+    name: "link",
+    label: "Liens",
+},{
+    type: "color",
+    name: "backgroundColor",
+    label: "Couleur de fond"
+},{
+    type: "checkbox",
+    name: "borderRadius",
+    label: "Bords arrondis",
+    valueTrue: "5px",
+    valueFalse: "0px"
+}])
 class Tooltip extends Component {
     constructor() {
         super("Tooltip", body, { });
