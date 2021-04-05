01596ff4d985208483274c7154dade5acc0a0ec1
ZAUB1 <aub1@protonmail.ch>
2021-04-05 19:58:49 +0200

    feature: picker button handlers

--- src/core/editor/comps/EditorPicker/index.ts
+++ src/core/editor/comps/EditorPicker/index.ts
@@ -1,7 +1,19 @@
 import "./style.scss";
 import body from "./body.html";
-import * as Nucleus from "../../../controllers/component";
 
+import * as Editor from "&/core/editor";
+import * as Nucleus from "&/core/controllers/component";
+import { ModelEventHandler } from "&/core/controllers/decorators/model";
+
+@ModelEventHandler("editBtn", "click", () => {
+})
+@ModelEventHandler("cloneBtn", "click", () => {
+})
+@ModelEventHandler("deleteBtn", "click", () => {
+    Editor.Instance.destroySelectedElem();
+    // Close UI
+    Editor.Instance.editorTools.close();
+})
 class EditorPicker extends Nucleus.Component {
     constructor() {
         super("EditorPicker", body, { });
