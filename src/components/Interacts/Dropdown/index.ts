eca33a4ca3dbc0704020e3b244a4e7b82b6cf89e
Aubin Luneau <aubin.luneau@icloud.com>
2021-04-06 13:54:03 +0200

    feature: dropdown

--- src/components/Interacts/Dropdown/index.ts
+++ src/components/Interacts/Dropdown/index.ts
@@ -1,14 +1,20 @@
 import body from "./body.html";
-// import style from "./style";
+import style from "./style";
 
 import { Component } from "../../../core/controllers/component";
 import { Category, Icon } from "../../../core/controllers/decorators/editor";
+import { ModelEventHandler } from "&/core/controllers/decorators/model";
 
 @Category("Interacts")
 @Icon("fas fa-square")
+@ModelEventHandler("dropBtn", "click", function() {
+    const content = this.getCompByModel("dropContent");
+    (content.style.visibility == "hidden") ? content.style.visibility = null : content.style.visibility = "visible";
+    (content.style.opacity == 1) ? content.style.opacity = 0 : content.style.opacity = 1;
+})
 class Dropdown extends Component {
     constructor() {
-        super("Dropdown", body, { });
+        super("Dropdown", body, { style });
     }
 };
 
--- src/components/Interacts/Dropdown/style.ts
+++ src/components/Interacts/Dropdown/style.ts
@@ -1,53 +1,14 @@
-// export default {
-//     "_nav ul": {
-//         width: "100%",
-//         padding: 0,
-//         margin: 0,
-//         listStyleType: "none"
-//     },
-    
-//     "_nav ul li": {
-//         width: "100%",
-//         display: "inline-block",
-//         backgroundColor: "#E64A19"
-//     },
-    
-//     "_nav a": {
-//         display: "block",
-//         padding: "0 10px",
-//         color: "#FFF",
-//         fontSize: "20px",
-//         lineHeight: "60px",
-//         textDecoration: "none"
-//     },
-    
-//     "nav a:hover": { 
-//         backgroundColor: "#000000"
-//     },
-    
-//     "nav ul ul": {
-//         display: "none",
-//         position: "absolute", 
-//         top: "60px" /* the height of the main nav */
-//     },
-
-//     "nav ul li:hover > ul": {
-//         display: "inherit",
-//     },
-    
-//     "nav ul ul li": {
-//         float: "none",
-//         display: "list-item",
-//         position: "relative"
-//     },
-    
-//     "nav ul ul ul li": {
-//         position: "relative",
-//         top: "-60px", 
-//         left: "170px"
-//     },
-    
-//     "_li > a:after": { content:  ' +' },
-//     "_li > a:only-child:after": { content: '' }
-    
-// }
+export default {
+    dropContainer: {
+        minWidth: "100px",
+        minHeight: "40px",
+        marginTop: "4px",
+        borderRadius: "5px",
+        boxShadow: "rgba(0, 0, 0, 0.12) 0px 16px 64px 0px, rgba(0, 0, 0, 0.01) 0px 0px 0px 1px",
+        transition: "all .1s ease-in-out",
+        opacity: 0,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+};
